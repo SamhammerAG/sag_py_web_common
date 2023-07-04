9c21d6b7973561e2eb2ba7e015b253f454c32330
Schulz <oschulz@samhammer.de>
2023-07-04 15:41:45 +0200

    KIT-2273 create new version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements-dev.txt", "r") as fin:
 
 setuptools.setup(
     name="sag-py-web-common",
-    version="0.1.1",
+    version="0.1.2",
     description="Small helper functions for web projects",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
