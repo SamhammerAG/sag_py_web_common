a16436b27befa26020e73c96708be1d4c180dec8
oschulz <oschulz@samhammer.de>
2024-04-30 18:07:22 +0200

    KIT-3396 increased version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements-dev.txt", "r") as fin:
 
 setuptools.setup(
     name="sag-py-web-common",
-    version="0.1.2",
+    version="0.1.3",
     description="Small helper functions for web projects",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
